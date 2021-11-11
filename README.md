31b338c6e1042fad0a25dbc6cd39cd923e6b9eea
Kent Joash Zamudio <kazamudio@up.edu.ph>
2021-11-11 23:15:29 +0800

    Working voting app. Needs some fixes on routing.

--- README.md
+++ README.md
@@ -1,70 +1,26 @@
-# Getting Started with Create React App
+# Description
+A Voting App implemented using React and Firebase. Created for Dev Jam.
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Demo 
+https://voting-app-devjam.web.app
 
-## Available Scripts
+# Features 
+1. User is shown multiple polls that they can choose from
+2. After clicking on a poll, user is shown a voting screen where they can vote on that poll
+3. Only authenticated users are allowed to vote
+4. Store items and votes in a database
+5. After voting, the user should see the results from everyone
+6. User can only vote once per poll
 
-In the project directory, you can run:
+# Gallery
+### Landing page
+![VOT Landing Page](./src/images/home.png)
 
-### `npm start`
+### Poll View
+![VOT Poll View](./src/images/poll-page.png)
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+### Poll Results View
+![VOT Poll Results View](./src/images/results.png)
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+# Disclaimer
+I am learning React from scratch again. i only used React hooks in this project so the state management is a bit messy. Also, please do not refresh the page when in `/poll` route since it has an unresolved error due to unmanaged state persistence, as I said, it is a TEEENY TINY BIT messy.
--- src/App.js
+++ src/App.js
@@ -1,24 +1,296 @@
-import logo from './logo.svg';
+//local files
+import firebaseConfig from './config';
+import logo from './images/vot.png';
 import './App.css';
 
+// firebase
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+//  react
+// import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect, useState } from 'react';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
+  // for App
+  // for auth
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      setUser(user);
+    });
+  });
+  // for selected poll
+  const [pollIndex, setPollIndex] = useState(null);
+  const selectedPoll = selectedIndex => {
+    setPollIndex(selectedIndex);
+  };
+  // for Polls
+  const pollsRef = firestore.collection('polls');
+  const query = pollsRef.limit(25);
+  const [polls] = useCollectionData(query, { idField: 'id' });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <div className="App hero is-info is-fullheight">
+        <Navbar user={user} />
+        <div class="hero-body p-0 pt-3 is-flex-direction-column">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Polls user={user} polls={polls} selectedPoll={selectedPoll} />
+              }
+            ></Route>
+            <Route
+              path="/poll"
+              element={<Poll user={user} polls={polls} pollIndex={pollIndex} />}
+            ></Route>
+            {/* <Route path="/signin" element={<SignInModal />}></Route> */}
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+function Polls({ user, polls, selectedPoll }) {
+  let navigate = useNavigate();
+  const handleSelected = e => {
+    selectedPoll(e.target.value);
+    // console.log("Target value: ", e.target.value)
+    navigate('/poll');
+  };
+  // console.log('Polls', polls);
+
+  return (
+    <>
+      <div class="mb-3 has-text-centered">
+        <p class="title">The Voting App</p>
+        <p class="subtitle">The largest collection of random polls</p>
+      </div>
+      <div class="columns is-centered">
+        <div class="column is-10">
+          <div class="columns is-multiline is-centered">
+            {polls &&
+              polls.map((poll, index) => (
+                <div class="column is-one-third" key={poll.id}>
+                  <div class="card is-flex is-flex-direction-column">
+                    <div class="card-image">
+                      <figure class="image is-3x2">
+                        <img src={poll.imgURL} alt="Poll Header" />
+                      </figure>
+                    </div>
+                    <div class="card-content">
+                      <p class="is-size-6 has-text-centered has-text-weight-bold">
+                        {poll.name}
+                      </p>
+                      <p class="is-size-7">{poll.description}</p>
+                    </div>
+                    <div class="card-footer mt-auto">
+                      <button
+                        class="card-footer-item button px-0 mx-3 is-info is-inverted has-text-weight-bold"
+                        value={index}
+                        onClick={handleSelected}
+                      >
+                        Vote →
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Poll({ user, polls, pollIndex }) {
+  let navigate = useNavigate();
+  // update document upon click
+  // eslint-disable-next-line
+  const updateFirestoreDoc = e => {
+    !user
+      ? // ? navigate('/signin')
+        signInWithGoogle()
+      : firestore
+          .collection('polls')
+          .doc(polls[pollIndex].id)
+          .update({
+            voters: [
+              ...polls[pollIndex].voters,
+              {
+                userid: auth.currentUser.uid,
+                vote: parseInt(e.target.value)
+              }
+            ]
+          });
+  };
+  const getUserVote = () => {
+    return polls[pollIndex].voters.filter(voter => voter.userid === user.uid);
+  };
+  const votePercentage = () => {
+    const totalVotes = polls[pollIndex].voters.length;
+    const options = Array(polls[pollIndex].options.length).fill(0);
+    polls[pollIndex].voters.forEach(voter => {
+      options[voter.vote]++;
+    });
+    return options.map(option => {
+      return `${((option / totalVotes) * 100).toFixed()}%`;
+    });
+  };
+  return (
+    <div>
+      <div class="mb-1 has-text-centered">
+        <p class="title">{polls[pollIndex].name}</p>
+        <p class="subtitle is-6">{polls[pollIndex].description}</p>
+      </div>
+      <div className="columns-is-centered">
+        <div className="card column">
+          <div className="card-image">
+            <figure className="image is-3x2">
+              <img src={polls[pollIndex].imgURL} alt="Poll Header" />
+            </figure>
+          </div>
+          <div className="card-content p-2">
+            {!polls[pollIndex].voters.some(voter => {
+              return voter.userid === user?.uid;
+            })
+              ? polls[pollIndex].options.map((option, index) => (
+                  <button
+                    className="button my-1 is-outlined is-fullwidth is-rounded is-info has-text-weight-bold"
+                    value={index}
+                    key={index}
+                    onClick={updateFirestoreDoc}
+                  >
+                    {option}
+                  </button>
+                ))
+              : polls[pollIndex].options.map((option, index) => (
+                  <div
+                    className="button is-justify-content-start is-fullwidth my-1 p-0"
+                    key={index}
+                    
+                  >
+                    <div
+                      className={getUserVote()[0].vote===index? "has-background-info": "has-background-grey"}
+                      style={{ width: votePercentage()[index], height: '100%' }}
+                    >
+                    </div>
+                    <span class="p-2 left is-7">{option}</span>
+                    <span class="p-2 right">{votePercentage()[index]}</span>
+                  </div>
+                ))}
+          </div>
+        </div>
+      </div>
+      <button
+        class="button my-3 is-info is-inverted bd-pagination-prev"
+        onClick={() => {
+          navigate('/');
+        }}
+      >
+        ← Back to Home
+      </button>
+    </div>
+  );
+}
+
+// function SignInModal() {
+//   // let navigate = useNavigate();
+//   const closeModal = () => {
+//     const signinModal = document.getElementById('signinModal');
+//     signinModal.classList.remove('is-active');
+//     // nextPath ? navigate(nextPath, { state: { pollId: pollId } }) : navigate('/');
+//     // navigate('/');
+//   };
+//   return (
+//     <div class={'modal is-active'} id="signinModal">
+//       <div class="modal-background"></div>
+//       <div class="modal-card">
+//         <header class="modal-card-head">
+//           <p class="modal-card-title">Sign in to vote</p>
+//           <button
+//             class="delete"
+//             aria-label="close"
+//             onClick={closeModal}
+//           ></button>
+//         </header>
+//         <section class="modal-card-body is-flex is-justify-content-center">
+//           <SignIn/>
+//         </section>
+//       </div>
+//     </div>
+//   );
+// }
+
+function Navbar({ user }) {
+  return (
+    <div class="hero-head">
+      <header class="navbar">
+        <div class="container is-flex is-fullwidth is-justify-content-space-between is-align-items-center">
+          <a class="navbar-item" href="/">
+            <img src={logo} alt="Logo" />
+          </a>
+          {user ? <SignOut user={user} /> : <SignIn />}
+        </div>
       </header>
     </div>
+  );
+}
+// for SignIn
+const signInWithGoogle = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  auth.signInWithPopup(provider);
+};
+
+// eslint-disable-next-line
+function SignIn() {
+  return (
+    <span class="navbar-item">
+      <button class="button is-info is-inverted" onClick={signInWithGoogle}>
+        <span class="icon">
+          <i class="fab fa-google"></i>
+        </span>
+        <span>Sign in with Google</span>
+      </button>
+    </span>
+  );
+}
+// eslint-disable-next-line
+function SignOut() {
+  let navigate = useNavigate();
+  return (
+    <span class="navbar-item">
+      <button
+        className="button is-info is-inverted"
+        onClick={() => {
+          auth.signOut();
+          navigate('/');
+        }}
+      >
+        <span class="icon">
+          <figure className="image is-1x1">
+            <img
+              class="is-rounded"
+              src={auth.currentUser.photoURL}
+              alt="User Profile"
+            />
+          </figure>
+        </span>
+        <span>Sign-out</span>
+      </button>
+    </span>
   );
 }
 
